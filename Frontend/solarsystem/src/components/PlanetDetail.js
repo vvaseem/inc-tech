583c6db322959f950803e2fb0634c39f96d8635f
Waseem Akhtar <w.a@outlook.com>
2023-06-18 00:11:36 +0100

    Formatted file using Prettier

--- Frontend/solarsystem/src/components/PlanetDetail.js
+++ Frontend/solarsystem/src/components/PlanetDetail.js
@@ -1,43 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import axios from 'axios'
+import axios from "axios";
 
 const PlanetDetail = () => {
-    const { id } = useParams()
-    const [planet, setPlanet] = useState([])
+  const { id } = useParams();
+  const [planet, setPlanet] = useState([]);
 
-    useEffect(() => {
-        axios.get(`https://localhost:7292/api/Planets/${id}`)
-            .then(response => {
-                console.log("PlanetDetail Response: ", response.data)
-                setPlanet(response.data)
-            })
-    }, [id])
-    
-    return (
+  useEffect(() => {
+    axios.get(`https://localhost:7292/api/Planets/${id}`).then((response) => {
+      setPlanet(response.data);
+    });
+  }, [id]);
+
+  return (
     <main>
-        <header>
-            <h1>{planet.name}</h1>
-        </header>
+      <header>
+        <h1>{planet.name}</h1>
+      </header>
 
-        <section>
-                <figure>
-                    <img src={planet.imageURL} alt={planet.name} />
-                    <figcaption>Surface of {planet.name}</figcaption>
-                </figure>
+      <section>
+        <figure>
+          <img src={planet.imageURL} alt={planet.name} />
+          <figcaption>Surface of {planet.name}</figcaption>
+        </figure>
 
-                <article>
-                    <p><strong>Distance from Sun:</strong> {planet.distanceFromSun}</p>
-                    <p><strong>Mass:</strong> {planet.mass}</p>
-                    <p><strong>Diameter:</strong> {planet.diameter}</p>
-                </article>
-            </section>
+        <article>
+          <p>
+            <strong>Distance from Sun:</strong> {planet.distanceFromSun}
+          </p>
+          <p>
+            <strong>Mass:</strong> {planet.mass}
+          </p>
+          <p>
+            <strong>Diameter:</strong> {planet.diameter}
+          </p>
+        </article>
+      </section>
 
-        <nav>
-            <Link to="/">Back Home</Link>
-        </nav>
+      <nav>
+        <Link to="/">Back Home</Link>
+      </nav>
     </main>
-  )
-}
+  );
+};
 
-export default PlanetDetail
+export default PlanetDetail;
--- Frontend/solarsystem/src/components/PlanetList.js
+++ Frontend/solarsystem/src/components/PlanetList.js
@@ -1,35 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
- const PlanetsList = () =>{
-    const [planets, setPlanets] = useState([]);
+const PlanetsList = () => {
+  const [planets, setPlanets] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://localhost:7292/api/Planets')
-            .then(response => {
-                setPlanets(response.data);
-                console.log("Planets: ", response.data);
-            })
-            .catch(error => console.error(`There was an error retrieving the list of planets: ${error}`));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("https://localhost:7292/api/Planets")
+      .then((response) => {
+        setPlanets(response.data);
+      })
+      .catch((error) =>
+        console.error(
+          `There was an error retrieving the list of planets: ${error}`
+        )
+      );
+  }, []);
 
-    return (
-        <main>
-            <header>
-                <h1>Names of Planets</h1>
-            </header>
-            
-            <section>
-                    {planets.map((planet, index) => (
-                        <div key={index} style={{margin: "0.5rem"}}>
-                            <Link to={`/planet/${planet.id}`}>{planet.name}</Link>
-                        </div>
-                    ))}
-            </section>
-        </main>
-    );
-    
-}
+  return (
+    <main>
+      <header>
+        <h1 style={{ color: "white" }}>Names of Planets</h1>
+      </header>
+
+      <section>
+        {planets.map((planet, index) => (
+          <div key={index} style={{ margin: "0.5rem" }}>
+            <Link to={`/planet/${planet.id}`}>{planet.name}</Link>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+};
 
 export default PlanetsList;

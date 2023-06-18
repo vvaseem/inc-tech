259b621f3aca5191cdc9c14483e1cf15c1c60961
Waseem Akhtar <w.a@outlook.com>
2023-06-18 18:06:45 +0100

    Added unit tests for infrastructure project

---
+++ Backend/SolarSystem.Backend/SolarSystem.Infrastructure.Tests/PlanetServiceTests.cs
@@ -0,0 +1,75 @@
+using FluentAssertions;
+using Moq;
+using SolarSystem.Domain.Entities;
+using SolarSystem.Domain.Interfaces;
+using SolarSystem.Infrastructure.Services;
+
+namespace SolarSystem.Infrastructure.Tests
+{
+    public class Tests
+    {
+        private Mock<IPlanetRepository> _planetRepositoryMock;
+        private PlanetService _planetService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _planetRepositoryMock = new Mock<IPlanetRepository>();
+            _planetService = new PlanetService(_planetRepositoryMock.Object);
+        }
+
+        [Test]
+        public void GetById_WhenPlanetExists_ReturnsCorrectPlanet()
+        {
+            // Arrange
+            var expectedPlanet = new Planet { Id = 1, Name = "Mercury" };
+            _planetRepositoryMock.Setup(x => x.GetById(It.IsAny<int>())).Returns(expectedPlanet);
+
+            // Act
+            var result = _planetService.GetById(1);
+
+            // Assert
+            result.Name.Should().Be(expectedPlanet.Name);
+            _planetRepositoryMock.Verify(x => x.GetById(1), Times.Once);
+        }
+
+        [Test]
+        public void GetById_WhenPlanetDoesNotExist_ReturnsNull()
+        {
+            // Arrange
+            _planetRepositoryMock.Setup(x => x.GetById(It.IsAny<int>())).Returns((Planet)null);
+
+            // Act
+            var result = _planetService.GetById(2);
+
+            // Assert
+            result.Should().BeNull();
+            _planetRepositoryMock.Verify(x => x.GetById(2), Times.Once);
+        }
+
+        [Test]
+        public void GetAll_WhenPlanetsExist_ReturnsAllPlanets()
+        {
+            // Arrange
+            var expectedPlanets = new List<Planet>
+        {
+            new Planet { Id = 1, Name = "Mercury" },
+            new Planet { Id = 2, Name = "Venus" },
+            new Planet { Id = 3, Name = "Earth" },
+            new Planet { Id = 4, Name = "Mars" },
+            new Planet { Id = 5, Name = "Jupiter" },
+            new Planet { Id = 6, Name = "Saturn" },
+            new Planet { Id = 7, Name = "Uranus" },
+            new Planet { Id = 8, Name = "Neptune" }
+        };
+            _planetRepositoryMock.Setup(x => x.GetAll()).Returns(expectedPlanets);
+
+            // Act
+            var result = _planetService.GetAll();
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedPlanets);
+            _planetRepositoryMock.Verify(x => x.GetAll(), Times.Once);
+        }
+    }
+}
